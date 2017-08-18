17c3def3abbaffb751905e25a63126473dfcbc39
German Gomez-Herrero <german@innovativetravel.eu>
2017-08-18 16:03:41 +0200

    DSE-159: Utility to create an Adwords click batch id

--- lib/udf_findhotel_utils.rb
+++ lib/udf_findhotel_utils.rb
@@ -45,6 +45,37 @@ class UdfFindhotelUtils
                            {query: "select ?('rooms', '2:0|1:2,6|2')", expect: 3, example: true},
                            {query: "select ?('roomz', '2:0|1:2,6|2')", expect: nil, example: true},
                        ]
+      },
+      {
+          type:        :function,
+          name:        :make_adwords_click_batch_id,
+          description: "Returns a unique identifier for a batch of clicks reported by Adwords.",
+          params:      "ad_group_id bigint, ad_id bigint, criteria_id bigint, device varchar(max), ad_network_type1 varchar(max), ad_network_type2 varchar(max), click_type varchar(max), slot varchar(max)",
+          return_type: "varchar(max)",
+          body:        %~
+            import hashlib
+            import json
+
+            key = {
+                "ad_group_id": ad_group_id,
+                "ad_id": ad_id,
+                "criteria_id": criteria_id,
+                "device": device,
+                "ad_network_type1": ad_network_type1,
+                "ad_network_type2": ad_network_type2,
+                "click_type": click_type,
+                "slot": slot}
+
+            m = hashlib.md5()
+            m.update(json.dumps(key, sort_keys=True))
+            return m.hexdigest()
+
+          ~,
+          tests:       [
+                           {query: "select ?(1, 2, 3, 'mobile', 'a', 'b', 's', 'x')", expect: 'd95633497fddd300103c07903b65533b' , example: true},
+                           {query: "select ?(3, 2, 1, 'mobile', 'a', 'b', 's', 'x')", expect: '78172d9b2a0d90cbff6160632e68449c' , example: true},
+                           {query: "select ?(1, 2, 3, 'mobile', 'b', 'a', 'h', 'y')", expect: '65db6fc8ffebab864baff57c4a90d5db' , example: true},
+                       ]
       }
     ]
 end
