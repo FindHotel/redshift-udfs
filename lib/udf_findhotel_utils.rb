3825106afb93e210c6acab1a0506e724ca00e312
rrh <raam@findhotel.net>
2017-09-20 14:04:31 +0200

    better name

--- lib/udf_findhotel_utils.rb
+++ lib/udf_findhotel_utils.rb
@@ -155,7 +155,7 @@ class UdfFindhotelUtils
       },
       {
           type:        :function,
-          name:        :parse_label_query_string,
+          name:        :qs_to_json,
           description: 'convert naked (without url) query string to json dict lifted from
                         https://github.com/awslabs/amazon-redshift-udfs/blob/master/scalar-udfs/f_parse_url_query_string.sql',
           params:      "url VARCHAR(MAX)",
@@ -165,7 +165,8 @@ class UdfFindhotelUtils
             import json
             return json.dumps(dict(parse_qsl(urlparse(url)[2])))
           ~,
-          tests:       [{query: "select parse_label_query_string('utf8=%E2%9C%93&query=redshift')", expect: '{"utf8": "\u2713", "query": "redshift"}' , example: true}]
+          tests:       [{query: "select parse_label_query_string('utf8=%E2%9C%93&query=redshift')", expect: '{"utf8": "\u2713", "query": "redshift"}' , example: true},
+                        {query: "select parse_label_query_string('http://example.com?utf8=%E2%9C%93&query=redshift')", expect: '{}' , example: true}]
       }
     ]
 end
